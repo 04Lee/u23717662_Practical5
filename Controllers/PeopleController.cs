597e642006dae455049f076e2ec28746e240b1b4
04Lee <u2317662@tuks.co.za>
2025-04-14 23:30:05 +0200

    Last last

--- Controllers/PeopleController.cs
+++ Controllers/PeopleController.cs
@@ -7,33 +7,26 @@ namespace NtandoMabuthoStudyGroupP2.Controllers
 {
     public class PeopleController : Controller
     {
-        // Simulated database list of students
         private static List<Student> studentList = new List<Student>();
 
-        // GET: People/Create
         public ActionResult Create()
         {
             return View();
         }
-
-        // POST: People/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Create(Student student)
         {
             if (ModelState.IsValid)
             {
-                // Add student to the list
                 studentList.Add(student);
-
-                // Redirect to display view
                 return RedirectToAction("StudentDisplay");
             }
 
             return View(student);
         }
 
-        // GET: People/StudentDisplay
+       
         public ActionResult StudentDisplay()
         {
             return View(studentList);
